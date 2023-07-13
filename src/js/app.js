57bc99ae7e2f4caf787048a8df42c4eeee644989
Pasha <pashahrytsyna@gmail.com>
2023-07-13 11:44:24 +0300

    add slider with basic animation, but not with delay for now

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,17 @@
-let a;
+const sliderImgs = document.querySelectorAll(".header__img");
+const sliderBtns = document.querySelectorAll(".header__slider-nav-item");
+
+console.log(sliderImgs, sliderBtns);
+
+sliderBtns.forEach((btn, i) => {
+  btn.addEventListener("click", (e) => changeSlide(e, i));
+});
+
+function changeSlide(event, i) {
+  sliderBtns.forEach((btn) =>
+    btn.classList.remove("header__slider-nav-item--active")
+  );
+  sliderImgs.forEach((img) => img.classList.remove("header__img--active"));
+  sliderBtns[i].classList.add("header__slider-nav-item--active");
+  sliderImgs[i].classList.add("header__img--active");
+}
